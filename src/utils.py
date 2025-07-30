0c673b703653ff052a2cb6c02f2c600913159eb6
chetandhaked <chetandhaked9828@gmail.com>
2025-07-30 11:25:27 +0530

    Pipline has done

--- src/utils.py
+++ src/utils.py
@@ -53,3 +53,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
 
     except Exception as e: # Catch the specific exception to log it and re-raise or handle properly
         raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
